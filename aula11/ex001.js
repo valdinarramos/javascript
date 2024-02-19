334902e78334e602ac79f976e5a399187fb34ad3
valdinarramos <137015855+valdinarramos@users.noreply.github.com>
2024-02-18 22:15:26 -0300

    aula 11

---
+++ aula11/ex001.js
@@ -0,0 +1,6 @@
+var vel = 58
+console.log(`Sua velocidade é de ${vel}km/h`)
+if( vel > 60){
+    console.log(`Você ultrapassou a velocidade maxima de 60km/h e foi MULTADO!`)
+}
+console.log('Dirija sempre usando cinto de segurança!')
---
+++ aula11/ex002.js
@@ -0,0 +1,7 @@
+var pais = 'brasil'
+console.log(`Seu pais é o ${pais}`)
+if(pais == 'Brasil'||'brasil'){
+    console.log('Você é Brasileiro')
+} else{
+    console.log('Você é Estrangeiro')
+}
