f5dd410dd0226a964d9547347eefbf67ce26b650
valdinarramos <137015855+valdinarramos@users.noreply.github.com>
2024-02-21 22:59:48 -0300

    Update script.js

--- pratica003/script.js
+++ pratica003/script.js
@@ -2,18 +2,23 @@ function contar(){
     var inicio = document.getElementById('inicio')
     var fim = document.getElementById('fim')
     var passo = document.getElementById('passo')
+    var res = document.getElementById('res')
     var i = Number(inicio.value)
     var f = Number(fim.value)
     var p = Number(passo.value)
-    if(i == 0 || f <= 1 ){
+    if(i == 0 || f == 0 || p ==0 ){
         alert('ERRO; verifique os dados e tente novamente')
-    } else if ( p == 0 ){
-        alert('Não é permitido passo 0, nesse caso sera considerado passo 1')
-        p = 1
+    }else{
+     res.innerHTML= 'Contando: '
+    if(i < f){
+        for(var c =i; c <= f; c += p){
+            res.innerHTML +=`${c} \u{1f449}`
+        }
+    } else{
+        for (var c = i; c>= f; c -= p ){
+            res.innerHTML +=`${c} \u{1f449}`
+        }
     }
-    var res = document.getElementById('res')
-    res.innerHTML = ''
-    for(var c = i; c <= f; c = c+p){
-        res.innerHTML += `\u{1F449} ${c} `
-    }
+    res.innerHTML += `\u{1f3c1}`
+}
 }
