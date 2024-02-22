397939c7094f662d84d8f4496434d62a14bb8cb3
valdinarramos <137015855+valdinarramos@users.noreply.github.com>
2024-02-22 10:12:34 -0300

    up

--- pratica004/script.js
+++ pratica004/script.js
@@ -1,14 +1,29 @@
 function gerar(){
     var n1 = document.getElementById('txt')
-    var n11= Number(n1.value)
-    var m = 0
-    var c = 0
-    var res = document.getElementById('res')
-    res.innerHTML = ''
+    var tab = document.getElementById('seltab')
+    
+    if(n1.value.length == 0){
+        alert('Por favor digite um numero para proseguir!')
+    }else{
+        var num= Number(n1.value)
+        var c = 0
+        tab.innerHTML = ''
+        while ( c <= 10){
+            var item = document.createElement('option')
+            item.text = `${num} x ${c} = ${num*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+        
+    }
+
+}
+
+
+/* res.innerHTML = ''
     do{
         c++
      m = n11*c
      res.innerHTML += `${n11} X ${c} = ${m} <br>`
-    }while(c <=10);
-
-}
+    }while(c <=10); */
