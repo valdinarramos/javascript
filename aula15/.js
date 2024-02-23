772366ebb59ed34b3b951a9ef6dabc335f555c1e
valdinarramos <137015855+valdinarramos@users.noreply.github.com>
2024-02-23 09:19:26 -0300

    aula 15 finalizado

---
+++ aula15/.js
@@ -0,0 +1,7 @@
+var num =[5,4,6,9,1,]
+var pos = num.indexOf(9)
+if (pos == -1){
+    console.log('o valor não foi encontrado!')
+}else{
+    console.log(`o valor está na posição ${pos+1}`)
+}
---
+++ aula15/ambiente02.js
@@ -0,0 +1,5 @@
+let valores = [2,1,4,6,8,3]
+valores.sort()
+for(let pos in valores){
+    console.log(`a posiçao ${pos} tem o valor ${valores[pos]}`)
+}
