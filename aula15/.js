8380fdbf2b3e30e6c05940cded9381fc9f21db19
valdinarramos <137015855+valdinarramos@users.noreply.github.com>
2024-02-23 23:40:33 -0300

    pratica 05

--- aula15/.js
+++ aula15/.js
@@ -1,7 +1,9 @@
-var num =[5,4,6,9,1,]
-var pos = num.indexOf(9)
+var num =[]
+num.push(9)
+num.push(2)
+var pos = num.indexOf(2)
 if (pos == -1){
     console.log('o valor não foi encontrado!')
 }else{
-    console.log(`o valor está na posição ${pos+1}`)
+    console.log(`o valor está na posição ${pos}`)
 }
---
+++ pratica005/script.js
@@ -0,0 +1,45 @@
+var c =[]
+var maior = 0 
+var menor = 100
+function adicionar(){
+var txt = document.getElementById('txt1')
+var tab = document.getElementById('txtres')
+var num = Number(txt.value)
+if(num <=0 || num > 100){
+    alert('ERRO! tente novamente!')
+}else{
+    var item = document.createElement('option')
+    item.text = `valor ${num} adicionado!`
+    item.value = `tab${num}}`
+    tab.appendChild(item)
+    c.push(num)       
+            
+    
+}
+
+}
+var media = 0
+function final(){
+    if(c==0){
+    alert('[ERRO] adicione numeros para finalizar')
+}else{
+    var txt2= document.getElementById('rel')
+    txt2.innerHTML= (`Ao todo, temos ${c.length} numeros cadastrados<br>`)
+    var soma = 0
+    for (var i = 0; i < c.length; i++) {
+        soma += c[i];
+        if(c[i] > maior){
+            maior = c[i]
+        }
+        if(c[i] < menor){
+            menor = c[i]
+        }
+        media= (soma/c.length)
+    }
+     txt2.innerHTML += (`O maior numero informado foi ${maior}<br>`)
+     txt2.innerHTML += (`O menor numero informado foi ${menor}<br>`)   
+     txt2.innerHTML += (`A soma dos valores é ${soma}<br>`)
+     txt2.innerHTML += (`A media dos valores é ${media}`)
+
+}
+}
